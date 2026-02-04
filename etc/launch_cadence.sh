0df14642447a72f4b2da67c0954b1b40b9d290c4
Mark Alea <markdaniel.alea@gmail.com>
2026-02-04 10:10:37 +0100

    added testboards

---
+++ etc/launch_cadence.sh
@@ -0,0 +1,11 @@
+export GF_PDK_HOME='/users/micas/micas/design/gf055bcdlite/55BCDlite/latest'
+export CDS_SEARCHDIR='$GF_PDK_HOME/DesignEnv/VirtuosoOA/setup'
+export GF_PDK_DOC_HOME='${GF_PDK_HOME}/doc'
+source /users/micas/micas/design/scripts/ic_23.10.020.rc
+#source /users/micas/micas/design/scripts/ic_6.1.8.310.rc
+source /users/micas/micas/design/scripts/calibre_2023.1_18.8.rc
+
+export MGLS_LICENSE_FILE=27003@license-extern.esat.kuleuven.be
+export SALT_LICENSE_SERVER=27003@license-extern.esat.kuleuven.be
+
+virtuoso &
